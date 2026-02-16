f6f1c2f41edad11fe3bc31f1f20d21359ad74f86
Cache Salyers <cachesalyers@gmail.com>
2026-02-15 19:56:15 -0600

    Initial Commit

---
+++ CalorieTracker/CalorieTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieTrackerApp.swift
+//  CalorieTracker
+//
+//  Created by Cache Salyers on 2/15/26.
+//
+
+import SwiftUI
+
+@main
+struct CalorieTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
